951aa4acab4b567e00a0d9f95d94770202675fa2
Ketosvaldo <ketchulo.conk2013@gmail.com>
2022-04-29 13:26:40 -0500

    Second commit

--- src/components/Todo/App.js
+++ src/components/Todo/App.js
@@ -1,16 +1,18 @@
-import logo from '../../logo.svg';
+import Header from '../../shared/layout/Header';
+import Footer from '../../shared/layout/Footer';
+import Content from '../../shared/layout/Content';
 import Timer from '../Pomodoro/Timer';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>
-          Header
-        </h1>
-      </header>
+      <Header title="Header" url="https://www.facebook.com"/>
+      <Content>
+        <Timer />
+      </Content>
+
+      <Footer/> 
     </div>
   );
 }
---
+++ src/shared/layout/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Footer = () => {
+    const root = {
+        marginTop: '20px',
+    }
+    return(
+        <footer style={root}>
+            Web2 {new Date().getFullYear()}
+        </footer>
+    )
+}
+
+export default Footer;
